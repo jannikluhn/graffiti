20a9608ae1725afd474a780d9bc5a0520f4c005c
Jannik Luhn <jannik@brainbot.com>
2021-02-06 14:57:27 +0100

    Goerli deployment

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,7 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
@@ -10,13 +8,24 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.6",
+  solidity: {
+    version: "0.7.6",
+    optimizer: {
+      enabled: true,
+      runs: 10000,
+    }
+  },
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/cb47771bf3324acc895994de6752654b",
+      accounts: [
+      ],
+    },
+  },
 };
 
