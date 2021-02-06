0e755b87febbc62f4fc49a5387edee633a994ca7
Jannik Luhn <jannik@brainbot.com>
2021-02-06 12:59:43 +0100

    Use bulma

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <h1 class="title">
+          Graffiti
+        </h1>
+        <h2 class="subtitle">
+          subtitle
+        </h2>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
