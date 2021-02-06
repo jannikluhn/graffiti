606a214a606ab9dcc4e5c837426cc9a97812b132
Jannik Luhn <jannik@brainbot.com>
2021-02-06 15:54:28 +0100

    Add basic graph

---
+++ graph-goerli/src/mapping.ts
@@ -0,0 +1,16 @@
+import { Bytes } from "@graphprotocol/graph-ts"
+import {
+  ColorChange,
+} from "../generated/Graffiti/Graffiti"
+import { Graffiti } from "../generated/schema"
+
+export function handleColorChange(event: ColorChange): void {
+  let graffiti = Graffiti.load("")
+  if (graffiti == null) {
+    graffiti = new Graffiti("");
+    graffiti.pixels = new Bytes(100);
+  }
+  let index = event.params.pixelID.toI32()
+  graffiti.pixels.fill(event.params.color, index, index + 1);
+  graffiti.save();
+}
