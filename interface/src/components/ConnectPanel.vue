96b67ca53390cccadc9bbcf4650073fbc39de900
Jannik Luhn <jannik@brainbot.com>
2021-02-08 12:44:38 +0100

    Fix address format

--- interface/src/components/ConnectPanel.vue
+++ interface/src/components/ConnectPanel.vue
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+import { ethers } from 'ethers'
+
 export default {
   name: 'ConnectPanel',
   data() {
@@ -35,13 +37,13 @@ export default {
       } catch(err) {
         this.$emit('error', 'Failed to requests accounts: ' + err.message)
       }
-      this.$emit("accountChanged", accounts[0])
+      this.$emit("accountChanged", ethers.utils.getAddress(accounts[0]))
       this.waitingForAccount = false
     },
 
     onAccountsChanged(accounts) {
       if (accounts.length >= 0) {
-        this.$emit("accountChanged", accounts[0])
+        this.$emit("accountChanged", ethers.utils.getAddress(accounts[0]))
       } else {
         this.$emit('error', 'No connected account.')
       }
