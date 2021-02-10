adda00e00a3344ab63db4818c31c12a42e2bcfd3
Jannik Luhn <jannik@brainbot.com>
2021-02-11 00:31:42 +0100

    About modal scaffold

---
+++ interface/src/components/AboutModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">About</p>
+        </header>
+
+        <section class="modal-card-body">
+          <p>About text</p>
+        </section>
+
+        <footer class="modal-card-foot">
+          <p>Footer</p>
+        </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutModal",
+}
+</script>
