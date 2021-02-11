d8fb044f17b4776d45a93008fe5b6eead0b67350
Maria <maria@brainbot.com>
2021-02-11 19:20:24 +0100

    finish about modal, add favicon and logo

--- interface/src/main.js
+++ interface/src/main.js
@@ -6,6 +6,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 
 import '@/assets/main.scss'
 
+
 if (window.ethereum) {
   const provider = new ethers.providers.Web3Provider(window.ethereum)
   const address = '0x86B6200E8A90603D20fb11C457ec6b8C4265C314'
