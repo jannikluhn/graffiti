3a19d9592809e5dabc724f45547d2abf608c2e88
Jannik Luhn <jannik@brainbot.com>
2021-05-08 14:37:07 +0200

    Add basics of modal

--- interface/src/components/AccountSidebarSection.vue
+++ interface/src/components/AccountSidebarSection.vue
@@ -1,15 +1,28 @@
 <template>
-  <SidebarSection title="Account"> </SidebarSection>
+  <SidebarSection title="Account">
+    <button @click="modalActive = true">Show Modal</button>
+    <Modal title="Modal" :active="modalActive" @close="modalActive = false">
+      <p>Modal body</p>
+    </Modal>
+  </SidebarSection>
 </template>
 
 <script>
 import SidebarSection from "./SidebarSection.vue";
+import Modal from "./Modal.vue";
 
 export default {
   name: "AccountSidebarSection",
 
+  data() {
+    return {
+      modalActive: false,
+    };
+  },
+
   components: {
     SidebarSection,
+    Modal,
   },
 };
 </script>
---
+++ interface/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="active" @click="onBackgroundClick" class="modal">
+    <div @click="onBodyClick" class="modal-body">
+      <h1>{{ title }}</h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["active", "title"],
+
+  methods: {
+    onBackgroundClick() {
+      this.$emit("close");
+    },
+    onBodyClick(event) {
+      event.stopPropagation();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/main.scss";
+
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: $modal-background-color;
+}
+
+.modal-body {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
+</style>
