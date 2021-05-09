a9a0b846978389bfcc28842a7dd9694f4e7f0dba
Jannik Luhn <jannik@brainbot.com>
2021-05-09 11:26:14 +0200

    Implement help section

--- interface/src/components/HelpSection.vue
+++ interface/src/components/HelpSection.vue
@@ -1,17 +1,46 @@
 <template>
   <SidebarSection>
-    <p>help</p>
+    <div>
+      <button @click="aboutModalActive = true">About</button>
+    </div>
+    <div>
+      <button @click="howtoModalActive = true">How-To</button>
+    </div>
+    <div>
+      DISCLAIMER: GraffitETH is unaudited. We think it's safe, but who the hell
+      knows.
+    </div>
+
+    <Modal
+      v-if="aboutModalActive"
+      title="About"
+      @close="aboutModalActive = false"
+    />
+    <Modal
+      v-else-if="howtoModalActive"
+      title="How-To"
+      @close="howtoModalActive = false"
+    />
   </SidebarSection>
 </template>
 
 <script>
 import SidebarSection from "./SidebarSection.vue";
+import Modal from "./Modal.vue";
 
 export default {
   name: "HelpSection",
 
   components: {
     SidebarSection,
+    Modal,
+  },
+
+  data() {
+    return {
+      aboutModalActive: false,
+      howtoModalActive: false,
+    };
   },
 };
 </script>
